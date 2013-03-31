2e004f321bdbfb3e24ebb06becbe3a7184dba720
Artyom Olshevskiy <siasiamail@gmail.com>
2013-03-31 03:31:56 +0300

    Moved to algs4 package

---
+++ algs4/src/main/java/com/coursera/algorithms/algs4/DoublingTest.java
@@ -0,0 +1,44 @@
+package com.coursera.algorithms.algs4;
+
+import com.coursera.algorithms.stdlib.*;
+import com.coursera.algorithms.stdlib.Stopwatch;
+
+/*************************************************************************
+ *  Compilation:  javac DoublingTest.java
+ *  Execution:    java DoublingTest
+ *  Dependencies: ThreeSum.java com.coursera.algorithms.stdlib.Stopwatch.java com.coursera.algorithms.stdlib.StdRandom.java com.coursera.algorithms.stdlib.StdOut.java
+ *
+ *  % java DoublingTest 
+ *      250   0.0
+ *      500   0.0
+ *     1000   0.1
+ *     2000   0.6
+ *     4000   4.5
+ *     8000  35.7
+ *  ...
+ *
+ *************************************************************************/
+
+public class DoublingTest {
+
+    // time ThreeSum.count() for N random 6-digit ints
+    public static double timeTrial(int N) {
+        int MAX = 1000000;
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(-MAX, MAX);
+        }
+        com.coursera.algorithms.stdlib.Stopwatch timer = new Stopwatch();
+        int cnt = ThreeSum.count(a);
+        return timer.elapsedTime();
+    }
+
+
+    public static void main(String[] args) { 
+        for (int N = 250; true; N += N) {
+            double time = timeTrial(N);
+            StdOut.printf("%7d %5.1f\n", N, time);
+        } 
+    } 
+} 
+
---
+++ algs4/src/main/java/com/coursera/algorithms/algs4/Whitelist.java
@@ -0,0 +1,47 @@
+package com.coursera.algorithms.algs4;
+
+import com.coursera.algorithms.stdlib.In;
+import com.coursera.algorithms.stdlib.StdIn;
+import com.coursera.algorithms.stdlib.StdOut;
+
+/*************************************************************************
+ *  Compilation:  javac Whitelist.java
+ *  Execution:    java Whitelist whitelist.txt < data.txt
+ *  Dependencies: StaticSetOfInts.java com.coursera.algorithms.stdlib.In.java com.coursera.algorithms.stdlib.StdOut.java
+ *  Data files:   http://algs4.cs.princeton.edu/11model/tinyW.txt
+ *                http://algs4.cs.princeton.edu/11model/tinyT.txt
+ *                http://algs4.cs.princeton.edu/11model/largeW.txt
+ *                http://algs4.cs.princeton.edu/11model/largeT.txt
+ *
+ *  Whitelist filter.
+ *
+ *
+ *  % java Whitelist tinyW.txt < tinyT.txt
+ *  50
+ *  99
+ *  13
+ *
+ *  % java Whitelist largeW.txt < largeT.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *
+ *************************************************************************/
+
+public class Whitelist {
+    public static void main(String[] args) {
+        int[] w = In.readInts(args[0]);
+        StaticSETofInts set = new StaticSETofInts(w);
+
+        // Read key, print if not in whitelist.
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (!set.contains(key))
+                StdOut.println(key);
+        }
+    }
+}
